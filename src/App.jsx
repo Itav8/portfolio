20b2104e7480cee1fcb7be0f303911bac583461f
Italiz <italizv@gmail.com>
2023-06-21 10:55:54 -0400

    Updated package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import { TextInput } from "../components/TextInput";
 
 function App() {
-  const [search, setSearch] = useState("");
-  const [text, setText] = useState("");
-
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
 
   return (
     <div className="App">
-      <form>
-        <div className="card">
-          <TextInput
-            handleChange={handleTextChange}
-            inputValue={text}
-            label="Text Input"
-          />
-
-          <TextInput
-            label="Search Input"
-            type="search"
-            handleChange={(event) => {
-              console.log(event);
-              setSearch(event.target.value);
-            }}
-            inputValue={search}
-          />
-          <button
-            onClick={(event) => {
-              event.preventDefault();
-              console.log("CLICKED!");
-            }}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
+      <h1>HELLO</h1>
     </div>
   );
 }
