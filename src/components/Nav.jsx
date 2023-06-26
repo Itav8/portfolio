7a445e88069e50ade638a0419973365ccf0985c2
Italiz <italizv@gmail.com>
2023-06-26 12:12:38 -0400

    Updated navbar with dropdown

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,29 @@
 import { NavLink } from "react-router-dom";
 
 import "../styles/Nav.css";
+import { useState } from "react";
 
 const Nav = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <nav className="navbar">
       <NavLink to="/">About Me</NavLink>
       <div className="navbar-right">
         <NavLink to="/resume">Resume</NavLink>
-        <div>
-          <a href="projects">Projects</a>
-          <ul class="dropdown">
-            <li>
+        <div className="dropdown" onClick={toggleDropdown}>
+          <button className="dropbutton">Projects</button>
+          {showDropdown && (
+            <div className="dropdown-content">
               <NavLink to="/trackly">Track.ly</NavLink>
-            </li>
-            <li>
               <NavLink to="/carcar">CarCar</NavLink>
-            </li>
-            <li>
               <NavLink to="/healthgpt">HealthGPT</NavLink>
-            </li>
-          </ul>
+            </div>
+          )}
         </div>
       </div>
     </nav>
