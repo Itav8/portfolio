4a10ccb04735d6f3d8f2208a270df39d188fddef
Italiz <italizv@gmail.com>
2023-06-27 15:44:39 -0400

    Added footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,6 +8,28 @@ const Layout = () => {
       <div className="main">
         <Outlet />
       </div>
+      <div>
+        <footer class="footer">
+          <a href="https://www.linkedin.com/in/italiz-vazquez/">
+            <img
+              src="https://img.icons8.com/?size=1x&id=xuvGCOXi8Wyg&format=png"
+              alt="LinkedIn"
+            />
+          </a>
+          <a href="https://github.com/Itav8">
+            <img
+              src="https://img.icons8.com/?size=1x&id=62856&format=png"
+              alt="GitHub"
+            />
+          </a>
+          <a href="https://gitlab.com/Itav8">
+            <img
+              src="https://img.icons8.com/?size=1x&id=epZz7YMDqqwA&format=png"
+              alt="GitLab"
+            />
+          </a>
+        </footer>
+      </div>
     </div>
   );
 };
