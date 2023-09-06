6754b47f89dc2bb30e10ac5bd246caa56ce8e06a
Italiz <italizv@gmail.com>
2023-09-06 16:30:48 -0400

    Updated about me page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,8 @@
 import Nav from "./Nav";
 import { Outlet } from "react-router-dom";
 
+import "../styles/Layout.css"
+
 const Layout = () => {
   return (
     <div>
@@ -9,22 +11,25 @@ const Layout = () => {
         <Outlet />
       </div>
       <div>
-        <footer class="footer">
+        <footer className="footer">
           <a href="https://www.linkedin.com/in/italiz-vazquez/">
             <img
-              src="https://img.icons8.com/?size=1x&id=xuvGCOXi8Wyg&format=png"
+              className="footer-icon"
+              src="https://img.icons8.com/?&id=xuvGCOXi8Wyg&format=png"
               alt="LinkedIn"
             />
           </a>
           <a href="https://github.com/Itav8">
             <img
-              src="https://img.icons8.com/?size=1x&id=62856&format=png"
+              className="footer-icon"
+              src="https://img.icons8.com/?&id=62856&format=png"
               alt="GitHub"
             />
           </a>
           <a href="https://gitlab.com/Itav8">
             <img
-              src="https://img.icons8.com/?size=1x&id=epZz7YMDqqwA&format=png"
+              className="footer-icon"
+              src="https://img.icons8.com/?&id=epZz7YMDqqwA&format=png"
               alt="GitLab"
             />
           </a>
