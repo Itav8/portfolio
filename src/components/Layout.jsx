d7107920a5249cc5305b18f0bd988ff4c40a87ef
Italiz <italizv@gmail.com>
2023-10-15 19:21:19 -0400

    Updated changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import Nav from "./Nav";
 import { Outlet } from "react-router-dom";
-import "../styles/Layout.css"
+import "../styles/Layout.css";
 
 const Layout = () => {
   return (
@@ -9,31 +9,29 @@ const Layout = () => {
       <div className="main">
         <Outlet />
       </div>
-      <div>
-        <footer className="footer">
-          <a href="https://www.linkedin.com/in/italiz-vazquez/">
-            <img
-              className="footer-icon"
-              src="https://img.icons8.com/?&id=xuvGCOXi8Wyg&format=png"
-              alt="LinkedIn"
-            />
-          </a>
-          <a href="https://github.com/Itav8">
-            <img
-              className="footer-icon"
-              src="https://img.icons8.com/?&id=62856&format=png"
-              alt="GitHub"
-            />
-          </a>
-          <a href="https://gitlab.com/kulixa">
-            <img
-              className="footer-icon"
-              src="https://img.icons8.com/?&id=epZz7YMDqqwA&format=png"
-              alt="GitLab"
-            />
-          </a>
-        </footer>
-      </div>
+      <footer className="footer">
+        <a href="https://www.linkedin.com/in/italiz-vazquez/">
+          <img
+            className="footer-icon"
+            src="https://img.icons8.com/?&id=xuvGCOXi8Wyg&format=png"
+            alt="LinkedIn"
+          />
+        </a>
+        <a href="https://github.com/Itav8">
+          <img
+            className="footer-icon"
+            src="https://img.icons8.com/?&id=62856&format=png"
+            alt="GitHub"
+          />
+        </a>
+        <a href="https://gitlab.com/kulixa">
+          <img
+            className="footer-icon"
+            src="https://img.icons8.com/?&id=epZz7YMDqqwA&format=png"
+            alt="GitLab"
+          />
+        </a>
+      </footer>
     </div>
   );
 };
