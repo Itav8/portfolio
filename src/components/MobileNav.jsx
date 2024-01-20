0c57b9adf675632404d23d2c4bf95e070ebdc108
Italiz <italizv@gmail.com>
2024-01-20 18:55:13 -0500

    Menu icon

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -6,11 +6,12 @@ import profileImg from "../images/profileImg.png";
 import "../styles/MobileNav.css";
 
 const MobileNav = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  // const [showDropdown, setShowDropdown] = useState(false);
 
-  const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
+  // const toggleDropdown = () => {
+  //   setShowDropdown(!showDropdown);
+  // };
 
   return (
     <nav className="mobile-navbar">
@@ -18,74 +19,91 @@ const MobileNav = () => {
         <img className="mobile-navbar-profile" alt="profile" src={profileImg} />
         Italiz Vazquez
       </NavLink>
-      <div className="mobile-navbar-right">
-        <NavLink to="/resume">Resume</NavLink>
-        <div className="mobile-navbar-dropdown">
-          <button
-            className="mobile-navbar-dropbutton"
-            onClick={toggleDropdown}
-            onBlur={() => {
-              setShowDropdown(false);
-            }}
+      <span
+        onClick={() => setMenuOpen(!menuOpen)}
+        className="mobile-navbar__hamburger"
+      >
+        menu
+      </span>
+      {menuOpen ? (
+        <>
+          <div
+            className="mobile-navbar__close"
+            onClick={() => setMenuOpen(false)}
           >
-            Projects
-          </button>
-          {showDropdown && (
-            <div className="dropdown-content">
-              <NavLink
-                className="dropdown-content__project"
-                to="/mimood"
-                onMouseDown={(e) => e.preventDefault()}
-                onClick={() => {
+            x
+          </div>
+          {}
+          {/* <div className="mobile-navbar-right">
+            <NavLink to="/resume">Resume</NavLink>
+            <div className="mobile-navbar-dropdown">
+              <button
+                className="mobile-navbar-dropbutton"
+                onClick={toggleDropdown}
+                onBlur={() => {
                   setShowDropdown(false);
                 }}
               >
-                MiMood
-              </NavLink>
-              <NavLink
-                className="dropdown-content__project"
-                to="/perfectlyplanned"
-                onMouseDown={(e) => e.preventDefault()}
-                onClick={() => {
-                  setShowDropdown(false);
-                }}
-              >
-                Perfectly Planned
-              </NavLink>
-              <NavLink
-                className="dropdown-content__project"
-                to="/healthgpt"
-                onMouseDown={(e) => e.preventDefault()}
-                onClick={() => {
-                  setShowDropdown(false);
-                }}
-              >
-                HealthGPT
-              </NavLink>
-              <NavLink
-                className="dropdown-content__project"
-                to="/carcar"
-                onMouseDown={(e) => e.preventDefault()}
-                onClick={() => {
-                  setShowDropdown(false);
-                }}
-              >
-                CarCar
-              </NavLink>
-              <NavLink
-                className="dropdown-content__project"
-                to="/trackly"
-                onMouseDown={(e) => e.preventDefault()}
-                onClick={() => {
-                  setShowDropdown(false);
-                }}
-              >
-                Track.ly
-              </NavLink>
-            </div>
-          )}
-        </div>
-      </div>
+                Projects
+              </button>
+              {showDropdown && (
+                <div className="dropdown-content">
+                  <NavLink
+                    className="dropdown-content__project"
+                    to="/mimood"
+                    onMouseDown={(e) => e.preventDefault()}
+                    onClick={() => {
+                      setShowDropdown(false);
+                    }}
+                  >
+                    MiMood
+                  </NavLink>
+                  <NavLink
+                    className="dropdown-content__project"
+                    to="/perfectlyplanned"
+                    onMouseDown={(e) => e.preventDefault()}
+                    onClick={() => {
+                      setShowDropdown(false);
+                    }}
+                  >
+                    Perfectly Planned
+                  </NavLink>
+                  <NavLink
+                    className="dropdown-content__project"
+                    to="/healthgpt"
+                    onMouseDown={(e) => e.preventDefault()}
+                    onClick={() => {
+                      setShowDropdown(false);
+                    }}
+                  >
+                    HealthGPT
+                  </NavLink>
+                  <NavLink
+                    className="dropdown-content__project"
+                    to="/carcar"
+                    onMouseDown={(e) => e.preventDefault()}
+                    onClick={() => {
+                      setShowDropdown(false);
+                    }}
+                  >
+                    CarCar
+                  </NavLink>
+                  <NavLink
+                    className="dropdown-content__project"
+                    to="/trackly"
+                    onMouseDown={(e) => e.preventDefault()}
+                    onClick={() => {
+                      setShowDropdown(false);
+                    }}
+                  >
+                    Track.ly
+                  </NavLink>
+                </div>
+              )}
+            </div> */}
+          {/* </div> */}
+        </>
+      ) : null}
     </nav>
   );
 };
